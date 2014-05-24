3219097f932169307d00d81b4ed4271a87aadaf1
Tim Plunkett <git@plnktt.com>
2014-05-23 21:38:30 -0700

    Issue #2273541 by tim.plunkett: Hide the block section of a page variant if no regions are provided.

--- src/Form/PageEditForm.php
+++ src/Form/PageEditForm.php
@@ -111,76 +111,77 @@ class PageEditForm extends PageFormBase {
       $form['page_variant_section']['page_variants'][$page_variant_id] = $row;
     }
 
-    $form['access_section_section'] = array(
-      '#type' => 'details',
-      '#title' => $this->t('Access Conditions'),
-      '#open' => TRUE,
-    );
-    $form['access_section_section']['add'] = array(
-      '#type' => 'link',
-      '#title' => $this->t('Add new access condition'),
-      '#route_name' => 'page_manager.access_condition_select',
-      '#route_parameters' => array(
-        'page' => $this->entity->id(),
-      ),
-      '#attributes' => $attributes,
-      '#attached' => array(
-        'library' => array(
-          'core/drupal.ajax',
-        ),
-      ),
-    );
-    $form['access_section_section']['access_section'] = array(
-      '#type' => 'table',
-      '#header' => array(
-        $this->t('Label'),
-        $this->t('Description'),
-        $this->t('Operations'),
-      ),
-      '#empty' => $this->t('There are no access conditions.'),
-    );
-
-    $form['access_section_section']['access_logic'] = array(
-      '#type' => 'radios',
-      '#options' => array(
-        'and' => $this->t('All conditions must pass'),
-        'or' => $this->t('Only one condition must pass'),
-      ),
-      '#default_value' => $this->entity->getAccessLogic(),
-    );
-
-    $access_conditions = $this->entity->getAccessConditions();
-    $form['access_section_section']['access'] = array(
-      '#tree' => TRUE,
-    );
-    foreach ($access_conditions as $access_id => $access_condition) {
-      $row = array();
-      $row['label']['#markup'] = $access_condition->getPluginDefinition()['label'];
-      $row['description']['#markup'] = $access_condition->summary();
-      $operations = array();
-      $operations['edit'] = array(
-        'title' => $this->t('Edit'),
-        'route_name' => 'page_manager.access_condition_edit',
-        'route_parameters' => array(
+    if ($access_conditions = $this->entity->getAccessConditions()) {
+      $form['access_section_section'] = array(
+        '#type' => 'details',
+        '#title' => $this->t('Access Conditions'),
+        '#open' => TRUE,
+      );
+      $form['access_section_section']['add'] = array(
+        '#type' => 'link',
+        '#title' => $this->t('Add new access condition'),
+        '#route_name' => 'page_manager.access_condition_select',
+        '#route_parameters' => array(
           'page' => $this->entity->id(),
-          'condition_id' => $access_id,
         ),
-        'attributes' => $attributes,
-      );
-      $operations['delete'] = array(
-        'title' => $this->t('Delete'),
-        'route_name' => 'page_manager.access_condition_delete',
-        'route_parameters' => array(
-          'page' => $this->entity->id(),
-          'condition_id' => $access_id,
+        '#attributes' => $attributes,
+        '#attached' => array(
+          'library' => array(
+            'core/drupal.ajax',
+          ),
         ),
-        'attributes' => $attributes,
       );
-      $row['operations'] = array(
-        '#type' => 'operations',
-        '#links' => $operations,
+      $form['access_section_section']['access_section'] = array(
+        '#type' => 'table',
+        '#header' => array(
+          $this->t('Label'),
+          $this->t('Description'),
+          $this->t('Operations'),
+        ),
+        '#empty' => $this->t('There are no access conditions.'),
       );
-      $form['access_section_section']['access_section'][$access_id] = $row;
+
+      $form['access_section_section']['access_logic'] = array(
+        '#type' => 'radios',
+        '#options' => array(
+          'and' => $this->t('All conditions must pass'),
+          'or' => $this->t('Only one condition must pass'),
+        ),
+        '#default_value' => $this->entity->getAccessLogic(),
+      );
+
+      $form['access_section_section']['access'] = array(
+        '#tree' => TRUE,
+      );
+      foreach ($access_conditions as $access_id => $access_condition) {
+        $row = array();
+        $row['label']['#markup'] = $access_condition->getPluginDefinition()['label'];
+        $row['description']['#markup'] = $access_condition->summary();
+        $operations = array();
+        $operations['edit'] = array(
+          'title' => $this->t('Edit'),
+          'route_name' => 'page_manager.access_condition_edit',
+          'route_parameters' => array(
+            'page' => $this->entity->id(),
+            'condition_id' => $access_id,
+          ),
+          'attributes' => $attributes,
+        );
+        $operations['delete'] = array(
+          'title' => $this->t('Delete'),
+          'route_name' => 'page_manager.access_condition_delete',
+          'route_parameters' => array(
+            'page' => $this->entity->id(),
+            'condition_id' => $access_id,
+          ),
+          'attributes' => $attributes,
+        );
+        $row['operations'] = array(
+          '#type' => 'operations',
+          '#links' => $operations,
+        );
+        $form['access_section_section']['access_section'][$access_id] = $row;
+      }
     }
     return $form;
   }
--- src/Form/PageVariantEditForm.php
+++ src/Form/PageVariantEditForm.php
@@ -44,123 +44,202 @@ class PageVariantEditForm extends PageVariantFormBase {
       )),
     );
 
-    // Build a table of all blocks used by this page variant.
-    $form['block_section'] = array(
-      '#type' => 'details',
-      '#title' => $this->t('Blocks'),
-      '#open' => TRUE,
-    );
-    $form['block_section']['add'] = array(
-      '#type' => 'link',
-      '#title' => $this->t('Add new block'),
-      '#route_name' => 'page_manager.page_variant_select_block',
-      '#route_parameters' => array(
-        'page' => $this->page->id(),
-        'page_variant_id' => $this->pageVariant->id(),
-      ),
-      '#attributes' => $attributes,
-      '#attached' => array(
-        'library' => array(
-          'core/drupal.ajax',
+    if ($block_assignments = $this->pageVariant->getRegionAssignments()) {
+      // Build a table of all blocks used by this page variant.
+      $form['block_section'] = array(
+        '#type' => 'details',
+        '#title' => $this->t('Blocks'),
+        '#open' => TRUE,
+      );
+      $form['block_section']['add'] = array(
+        '#type' => 'link',
+        '#title' => $this->t('Add new block'),
+        '#route_name' => 'page_manager.page_variant_select_block',
+        '#route_parameters' => array(
+          'page' => $this->page->id(),
+          'page_variant_id' => $this->pageVariant->id(),
         ),
-      ),
-    );
-    $form['block_section']['blocks'] = array(
-      '#type' => 'table',
-      '#header' => array(
-        $this->t('Label'),
-        $this->t('Plugin ID'),
-        $this->t('Region'),
-        $this->t('Weight'),
-        $this->t('Operations'),
-      ),
-      '#empty' => $this->t('There are no regions for blocks.'),
-    );
-    // Loop through the blocks per region.
-    foreach ($this->pageVariant->getRegionAssignments() as $region => $blocks) {
-      // Add a section for each region and allow blocks to be dragged between
-      // them.
-      $form['block_section']['blocks']['#tabledrag'][] = array(
-        'action' => 'match',
-        'relationship' => 'sibling',
-        'group' => 'block-region-select',
-        'subgroup' => 'block-region-' . $region,
-        'hidden' => FALSE,
-      );
-      $form['block_section']['blocks']['#tabledrag'][] = array(
-        'action' => 'order',
-        'relationship' => 'sibling',
-        'group' => 'block-weight',
-        'subgroup' => 'block-weight-' . $region,
-      );
-      $form['block_section']['blocks'][$region] = array(
-        '#attributes' => array(
-          'class' => array('region-title', 'region-title-' . $region),
-          'no_striping' => TRUE,
-        ),
-      );
-      $form['block_section']['blocks'][$region]['title'] = array(
-        '#markup' => $this->pageVariant->getRegionName($region),
-        '#wrapper_attributes' => array(
-          'colspan' => 5,
-        ),
-      );
-      $form['block_section']['blocks'][$region . '-message'] = array(
-        '#attributes' => array(
-          'class' => array(
-            'region-message',
-            'region-' . $region . '-message',
-            empty($blocks) ? 'region-empty' : 'region-populated',
+        '#attributes' => $attributes,
+        '#attached' => array(
+          'library' => array(
+            'core/drupal.ajax',
           ),
         ),
       );
-      $form['block_section']['blocks'][$region . '-message']['message'] = array(
-        '#markup' => '<em>' . t('No blocks in this region') . '</em>',
-        '#wrapper_attributes' => array(
-          'colspan' => 5,
+      $form['block_section']['blocks'] = array(
+        '#type' => 'table',
+        '#header' => array(
+          $this->t('Label'),
+          $this->t('Plugin ID'),
+          $this->t('Region'),
+          $this->t('Weight'),
+          $this->t('Operations'),
         ),
+        '#empty' => $this->t('There are no regions for blocks.'),
+      );
+      // Loop through the blocks per region.
+      foreach ($block_assignments as $region => $blocks) {
+        // Add a section for each region and allow blocks to be dragged between
+        // them.
+        $form['block_section']['blocks']['#tabledrag'][] = array(
+          'action' => 'match',
+          'relationship' => 'sibling',
+          'group' => 'block-region-select',
+          'subgroup' => 'block-region-' . $region,
+          'hidden' => FALSE,
+        );
+        $form['block_section']['blocks']['#tabledrag'][] = array(
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'block-weight',
+          'subgroup' => 'block-weight-' . $region,
+        );
+        $form['block_section']['blocks'][$region] = array(
+          '#attributes' => array(
+            'class' => array('region-title', 'region-title-' . $region),
+            'no_striping' => TRUE,
+          ),
+        );
+        $form['block_section']['blocks'][$region]['title'] = array(
+          '#markup' => $this->pageVariant->getRegionName($region),
+          '#wrapper_attributes' => array(
+            'colspan' => 5,
+          ),
+        );
+        $form['block_section']['blocks'][$region . '-message'] = array(
+          '#attributes' => array(
+            'class' => array(
+              'region-message',
+              'region-' . $region . '-message',
+              empty($blocks) ? 'region-empty' : 'region-populated',
+            ),
+          ),
+        );
+        $form['block_section']['blocks'][$region . '-message']['message'] = array(
+          '#markup' => '<em>' . t('No blocks in this region') . '</em>',
+          '#wrapper_attributes' => array(
+            'colspan' => 5,
+          ),
+        );
+
+        /** @var $blocks \Drupal\block\BlockPluginInterface[] */
+        foreach ($blocks as $block_id => $block) {
+          $row = array(
+            '#attributes' => array(
+              'class' => array('draggable'),
+            ),
+          );
+          $row['label']['#markup'] = $block->label();
+          $row['id']['#markup'] = $block->getPluginId();
+          // Allow the region to be changed for each block.
+          $row['region'] = array(
+            '#title' => $this->t('Region'),
+            '#title_display' => 'invisible',
+            '#type' => 'select',
+            '#options' => $this->pageVariant->getRegionNames(),
+            '#default_value' => $this->pageVariant->getRegionAssignment($block_id),
+            '#attributes' => array(
+              'class' => array('block-region-select', 'block-region-' . $region),
+            ),
+          );
+          // Allow the weight to be changed for each block.
+          $configuration = $block->getConfiguration();
+          $row['weight'] = array(
+            '#type' => 'weight',
+            '#default_value' => isset($configuration['weight']) ? $configuration['weight'] : 0,
+            '#title' => t('Weight for @block block', array('@block' => $block->label())),
+            '#title_display' => 'invisible',
+            '#attributes' => array(
+              'class' => array('block-weight', 'block-weight-' . $region),
+            ),
+          );
+          // Add the operation links.
+          $operations = array();
+          $operations['edit'] = array(
+            'title' => $this->t('Edit'),
+            'route_name' => 'page_manager.page_variant_edit_block',
+            'route_parameters' => array(
+              'page' => $this->page->id(),
+              'page_variant_id' => $this->pageVariant->id(),
+              'block_id' => $block_id,
+            ),
+            'attributes' => $attributes,
+          );
+          $row['operations'] = array(
+            '#type' => 'operations',
+            '#links' => $operations,
+          );
+          $form['block_section']['blocks'][$block_id] = $row;
+        }
+      }
+    }
+
+    if ($selection_conditions = $this->pageVariant->getSelectionConditions()) {
+      // Selection conditions.
+      $form['selection_section'] = array(
+        '#type' => 'details',
+        '#title' => $this->t('Selection Conditions'),
+        '#open' => TRUE,
+      );
+      $form['selection_section']['add'] = array(
+        '#type' => 'link',
+        '#title' => $this->t('Add new selection condition'),
+        '#route_name' => 'page_manager.selection_condition_select',
+        '#route_parameters' => array(
+          'page' => $this->page->id(),
+          'page_variant_id' => $this->pageVariant->id(),
+        ),
+        '#attributes' => $attributes,
+        '#attached' => array(
+          'library' => array(
+            'core/drupal.ajax',
+          ),
+        ),
+      );
+      $form['selection_section']['table'] = array(
+        '#type' => 'table',
+        '#header' => array(
+          $this->t('Label'),
+          $this->t('Description'),
+          $this->t('Operations'),
+        ),
+        '#empty' => $this->t('There are no selection conditions.'),
       );
 
-      /** @var $blocks \Drupal\block\BlockPluginInterface[] */
-      foreach ($blocks as $block_id => $block) {
-        $row = array(
-          '#attributes' => array(
-            'class' => array('draggable'),
-          ),
-        );
-        $row['label']['#markup'] = $block->label();
-        $row['id']['#markup'] = $block->getPluginId();
-        // Allow the region to be changed for each block.
-        $row['region'] = array(
-          '#title' => $this->t('Region'),
-          '#title_display' => 'invisible',
-          '#type' => 'select',
-          '#options' => $this->pageVariant->getRegionNames(),
-          '#default_value' => $this->pageVariant->getRegionAssignment($block_id),
-          '#attributes' => array(
-            'class' => array('block-region-select', 'block-region-' . $region),
-          ),
-        );
-        // Allow the weight to be changed for each block.
-        $configuration = $block->getConfiguration();
-        $row['weight'] = array(
-          '#type' => 'weight',
-          '#default_value' => isset($configuration['weight']) ? $configuration['weight'] : 0,
-          '#title' => t('Weight for @block block', array('@block' => $block->label())),
-          '#title_display' => 'invisible',
-          '#attributes' => array(
-            'class' => array('block-weight', 'block-weight-' . $region),
-          ),
-        );
-        // Add the operation links.
+      $form['selection_section']['selection_logic'] = array(
+        '#type' => 'radios',
+        '#options' => array(
+          'and' => $this->t('All conditions must pass'),
+          'or' => $this->t('Only one condition must pass'),
+        ),
+        '#default_value' => $this->pageVariant->getSelectionLogic(),
+      );
+
+      $form['selection_section']['selection'] = array(
+        '#tree' => TRUE,
+      );
+      foreach ($selection_conditions as $selection_id => $selection_condition) {
+        $row = array();
+        $row['label']['#markup'] = $selection_condition->getPluginDefinition()['label'];
+        $row['description']['#markup'] = $selection_condition->summary();
         $operations = array();
         $operations['edit'] = array(
           'title' => $this->t('Edit'),
-          'route_name' => 'page_manager.page_variant_edit_block',
+          'route_name' => 'page_manager.selection_condition_edit',
           'route_parameters' => array(
             'page' => $this->page->id(),
             'page_variant_id' => $this->pageVariant->id(),
-            'block_id' => $block_id,
+            'condition_id' => $selection_id,
+          ),
+          'attributes' => $attributes,
+        );
+        $operations['delete'] = array(
+          'title' => $this->t('Delete'),
+          'route_name' => 'page_manager.selection_condition_delete',
+          'route_parameters' => array(
+            'page' => $this->page->id(),
+            'page_variant_id' => $this->pageVariant->id(),
+            'condition_id' => $selection_id,
           ),
           'attributes' => $attributes,
         );
@@ -168,84 +247,8 @@ class PageVariantEditForm extends PageVariantFormBase {
           '#type' => 'operations',
           '#links' => $operations,
         );
-        $form['block_section']['blocks'][$block_id] = $row;
+        $form['selection_section']['table'][$selection_id] = $row;
       }
-    }
-
-    // Selection conditions.
-    $form['selection_section'] = array(
-      '#type' => 'details',
-      '#title' => $this->t('Selection Conditions'),
-      '#open' => TRUE,
-    );
-    $form['selection_section']['add'] = array(
-      '#type' => 'link',
-      '#title' => $this->t('Add new selection condition'),
-      '#route_name' => 'page_manager.selection_condition_select',
-      '#route_parameters' => array(
-        'page' => $this->page->id(),
-        'page_variant_id' => $this->pageVariant->id(),
-      ),
-      '#attributes' => $attributes,
-      '#attached' => array(
-        'library' => array(
-          'core/drupal.ajax',
-        ),
-      ),
-    );
-    $form['selection_section']['table'] = array(
-      '#type' => 'table',
-      '#header' => array(
-        $this->t('Label'),
-        $this->t('Description'),
-        $this->t('Operations'),
-      ),
-      '#empty' => $this->t('There are no selection conditions.'),
-    );
-
-    $form['selection_section']['selection_logic'] = array(
-      '#type' => 'radios',
-      '#options' => array(
-        'and' => $this->t('All conditions must pass'),
-        'or' => $this->t('Only one condition must pass'),
-      ),
-      '#default_value' => $this->pageVariant->getSelectionLogic(),
-    );
-
-    $selection_conditions = $this->pageVariant->getSelectionConditions();
-    $form['selection_section']['selection'] = array(
-      '#tree' => TRUE,
-    );
-    foreach ($selection_conditions as $selection_id => $selection_condition) {
-      $row = array();
-      $row['label']['#markup'] = $selection_condition->getPluginDefinition()['label'];
-      $row['description']['#markup'] = $selection_condition->summary();
-      $operations = array();
-      $operations['edit'] = array(
-        'title' => $this->t('Edit'),
-        'route_name' => 'page_manager.selection_condition_edit',
-        'route_parameters' => array(
-          'page' => $this->page->id(),
-          'page_variant_id' => $this->pageVariant->id(),
-          'condition_id' => $selection_id,
-        ),
-        'attributes' => $attributes,
-      );
-      $operations['delete'] = array(
-        'title' => $this->t('Delete'),
-        'route_name' => 'page_manager.selection_condition_delete',
-        'route_parameters' => array(
-          'page' => $this->page->id(),
-          'page_variant_id' => $this->pageVariant->id(),
-          'condition_id' => $selection_id,
-        ),
-        'attributes' => $attributes,
-      );
-      $row['operations'] = array(
-        '#type' => 'operations',
-        '#links' => $operations,
-      );
-      $form['selection_section']['table'][$selection_id] = $row;
     }
 
     return $form;
