d920b609abe8dba8c763612edff16e664a17ac8e
berdir <berdir@214652.no-reply.drupal.org>
2014-06-16 12:07:27 -0700

    Issue #2287099 by Berdir: Limit the Entity view block to entity types with a view builder.

--- src/Plugin/Deriver/EntityViewDeriver.php
+++ src/Plugin/Deriver/EntityViewDeriver.php
@@ -51,13 +51,15 @@ class EntityViewDeriver extends DerivativeBase implements ContainerDerivativeInt
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
     foreach ($this->entityManager->getDefinitions() as $entity_type_id => $entity_type) {
-      $this->derivatives[$entity_type_id] = $base_plugin_definition;
-      $this->derivatives[$entity_type_id]['admin_label'] = $this->t('Entity view (@label)', array('@label' => $entity_type->getLabel()));
-      $this->derivatives[$entity_type_id]['context'] = array(
-        'entity' => array(
-          'type' => 'entity:' . $entity_type_id,
-        ),
-      );
+      if ($entity_type->hasViewBuilderClass()) {
+        $this->derivatives[$entity_type_id] = $base_plugin_definition;
+        $this->derivatives[$entity_type_id]['admin_label'] = $this->t('Entity view (@label)', array('@label' => $entity_type->getLabel()));
+        $this->derivatives[$entity_type_id]['context'] = array(
+          'entity' => array(
+            'type' => 'entity:' . $entity_type_id,
+          ),
+        );
+      }
     }
     return $this->derivatives;
   }
