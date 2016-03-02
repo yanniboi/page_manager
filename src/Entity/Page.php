d2c61c4dbb3a2435b355136c85bcbd846a2f3f41
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2016-03-02 13:15:24 +0000

    by yanniboi: Use abstract display and display variant config entities.

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -7,14 +7,9 @@
 
 namespace Drupal\page_manager\Entity;
 
-use Drupal\Component\Plugin\Context\ContextInterface;
-use Drupal\page_manager\Event\PageManagerContextEvent;
-use Drupal\page_manager\Event\PageManagerEvents;
 use Drupal\page_manager\PageInterface;
-use Drupal\Core\Condition\ConditionPluginCollection;
-use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\panels\Entity\DisplayBase;
 use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\page_manager\PageVariantInterface;
 
 /**
  * Defines a Page entity class.
@@ -42,63 +37,7 @@ use Drupal\page_manager\PageVariantInterface;
  *   },
  * )
  */
-class Page extends ConfigEntityBase implements PageInterface {
-
-  /**
-   * The ID of the page entity.
-   *
-   * @var string
-   */
-  protected $id;
-
-  /**
-   * The label of the page entity.
-   *
-   * @var string
-   */
-  protected $label;
-
-  /**
-   * The path of the page entity.
-   *
-   * @var string
-   */
-  protected $path;
-
-  /**
-   * The page variant entities.
-   *
-   * @var \Drupal\page_manager\PageVariantInterface[].
-   */
-  protected $variants;
-
-  /**
-   * An array of collected contexts.
-   *
-   * @var \Drupal\Component\Plugin\Context\ContextInterface[]
-   */
-  protected $contexts = [];
-
-  /**
-   * The configuration of access conditions.
-   *
-   * @var array
-   */
-  protected $access_conditions = [];
-
-  /**
-   * Tracks the logic used to compute access, either 'and' or 'or'.
-   *
-   * @var string
-   */
-  protected $access_logic = 'and';
-
-  /**
-   * The plugin collection that holds the access conditions.
-   *
-   * @var \Drupal\Component\Plugin\LazyPluginCollection
-   */
-  protected $accessConditionCollection;
+class Page extends DisplayBase implements PageInterface {
 
   /**
    * Indicates if this page should be displayed in the admin theme.
@@ -106,19 +45,6 @@ class Page extends ConfigEntityBase implements PageInterface {
    * @var bool
    */
   protected $use_admin_theme;
-
-  /**
-   * Parameter context configuration.
-   *
-   * An associative array keyed by parameter name, which contains associative
-   * arrays with the following keys:
-   * - machine_name: Machine-readable context name.
-   * - label: Human-readable context name.
-   * - type: Context type.
-   *
-   * @var array[]
-   */
-  protected $parameters = [];
 
   /**
    * {@inheritdoc}
@@ -132,91 +58,6 @@ class Page extends ConfigEntityBase implements PageInterface {
    */
   public function usesAdminTheme() {
     return isset($this->use_admin_theme) ? $this->use_admin_theme : strpos($this->getPath(), '/admin/') === 0;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function postSave(EntityStorageInterface $storage, $update = TRUE) {
-    parent::postSave($storage, $update);
-    static::routeBuilder()->setRebuildNeeded();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function postDelete(EntityStorageInterface $storage, array $entities) {
-    parent::postDelete($storage, $entities);
-    static::routeBuilder()->setRebuildNeeded();
-  }
-
-  /**
-   * Wraps the route builder.
-   *
-   * @return \Drupal\Core\Routing\RouteBuilderInterface
-   *   An object for state storage.
-   */
-  protected static function routeBuilder() {
-    return \Drupal::service('router.builder');
-  }
-
-  /**
-   * Wraps the entity storage for page variants.
-   *
-   * @return \Drupal\Core\Entity\EntityStorageInterface
-   */
-  protected function variantStorage() {
-    return \Drupal::service('entity_type.manager')->getStorage('page_variant');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getPluginCollections() {
-    return [
-      'access_conditions' => $this->getAccessConditions(),
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getAccessConditions() {
-    if (!$this->accessConditionCollection) {
-      $this->accessConditionCollection = new ConditionPluginCollection(\Drupal::service('plugin.manager.condition'), $this->get('access_conditions'));
-    }
-    return $this->accessConditionCollection;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function addAccessCondition(array $configuration) {
-    $configuration['uuid'] = $this->uuidGenerator()->generate();
-    $this->getAccessConditions()->addInstanceId($configuration['uuid'], $configuration);
-    return $configuration['uuid'];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getAccessCondition($condition_id) {
-    return $this->getAccessConditions()->get($condition_id);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function removeAccessCondition($condition_id) {
-    $this->getAccessConditions()->removeInstanceId($condition_id);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getAccessLogic() {
-    return $this->access_logic;
   }
 
   /**
@@ -290,79 +131,6 @@ class Page extends ConfigEntityBase implements PageInterface {
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public function addContext($name, ContextInterface $value) {
-    $this->contexts[$name] = $value;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getContexts() {
-    if (!$this->contexts) {
-      $this->eventDispatcher()->dispatch(PageManagerEvents::PAGE_CONTEXT, new PageManagerContextEvent($this));
-    }
-    return $this->contexts;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function addVariant(PageVariantInterface $variant) {
-    $this->variants[$variant->id()] = $variant;
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getVariant($variant_id) {
-    $variants = $this->getVariants();
-    if (!isset($variants[$variant_id])) {
-      throw new \UnexpectedValueException('The requested variant does not exist or is not associated with this page');
-    }
-    return $variants[$variant_id];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function removeVariant($variant_id) {
-    $this->getVariant($variant_id)->delete();
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getVariants() {
-    if (!isset($this->variants)) {
-      $this->variants = [];
-      /** @var \Drupal\page_manager\PageVariantInterface $variant */
-      foreach ($this->variantStorage()->loadByProperties(['page' => $this->id()]) as $variant) {
-        $this->variants[$variant->id()] = $variant;
-      }
-      // Suppress errors because of https://bugs.php.net/bug.php?id=50688.
-      @uasort($this->variants, [$this, 'variantSortHelper']);
-    }
-    return $this->variants;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function variantSortHelper($a, $b) {
-    $a_weight = $a->getWeight();
-    $b_weight = $b->getWeight();
-    if ($a_weight == $b_weight) {
-      return 0;
-    }
-
-    return ($a_weight < $b_weight) ? -1 : 1;
-  }
-
-  /**
    * Wraps the event dispatcher.
    *
    * @return \Symfony\Component\EventDispatcher\EventDispatcherInterface
@@ -370,33 +138,6 @@ class Page extends ConfigEntityBase implements PageInterface {
    */
   protected function eventDispatcher() {
     return \Drupal::service('event_dispatcher');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __sleep() {
-    $vars = parent::__sleep();
-
-    // Ensure any plugin collections are stored correctly before serializing.
-    // @todo Let https://www.drupal.org/node/2650588 handle this instead.
-    foreach ($this->getPluginCollections() as $plugin_config_key => $plugin_collection) {
-      $this->set($plugin_config_key, $plugin_collection->getConfiguration());
-    }
-
-    // Avoid serializing plugin collections and entities as they might contain
-    // references to a lot of objects including the container.
-    $unset_vars = [
-      'variants',
-      'accessConditionCollection',
-    ];
-    foreach ($unset_vars as $unset_var) {
-      if (!empty($this->{$unset_var})) {
-        unset($vars[array_search($unset_var, $vars)]);
-      }
-    }
-
-    return $vars;
   }
 
 }
