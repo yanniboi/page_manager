3284b36d6e13c13bdd68c6e96cc638401e409f67
yanniboi <yan@yanniboi.com>
2016-03-15 16:51:58 +0000

    hangouts dev by EclipseGc.

--- page_manager_ui/src/Form/PageParametersForm.php
+++ page_manager_ui/src/Form/PageParametersForm.php
@@ -90,11 +90,11 @@ class PageParametersForm extends FormBase {
     $cached_values = $form_state->getTemporaryValue('wizard');
 
     /** @var \Drupal\Core\StringTranslation\TranslatableMarkup $add */
-    $add = $form_state->getValue('add');
+    /*$add = $form_state->getValue('add');
     if ($add->getUntranslatedString() == 'Add new context') {
       list(, $route_parameters) = $this->getOperationsRouteInfo($cached_values, $this->machine_name, $form_state->getValue('types'));
       $form_state->setRedirect($this->getAddRoute($cached_values), $route_parameters);
-    }
+    }*/
   }
 
   protected function getOperations($route_name_base, array $route_parameters = array()) {
